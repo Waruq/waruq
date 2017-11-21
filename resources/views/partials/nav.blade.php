8d8a76859ebd720cd2146f241e145054a07d83f5
Sheikh Taha <tahalatief@gmail.com>
2017-11-21 23:31:57 +0530

    Had to Add new nav partial for Service Page

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,96 @@
+    <!-- Navigation panel -->
+            <nav class="main-nav js-stick">
+                <div class="full-wrapper relative clearfix">
+                    <!-- Logo ( * your text or image into link tag *) -->
+                    <div class="nav-logo-wrap local-scroll">
+                        <a href="mp-index.html" class="logo">
+                            <img src="images/logo-dark.png" alt="" />
+                        </a>
+                    </div>
+                    <div class="mobile-nav">
+                        <i class="fa fa-bars"></i>
+                    </div>
+                    
+                   <!-- Main Menu -->
+                    <div class="inner-nav desktop-nav">
+                        <ul class="clearlist">
+                            
+                            <!-- Item With Sub -->
+                            <li>
+                                <a href="/home" class="mn-has-sub active">Home </a>
+                                
+                            
+                                
+                            </li>
+                            <!-- End Item With Sub -->
+							
+							<!-- Item With Sub -->
+                            <li>
+                                <a href="/about" class="mn-has-sub ">About Us </a>                        
+                            </li>
+                            <!-- End Item With Sub -->
+							
+							<!-- Item With Sub -->
+                            <li>
+                                <a href="/services" class="mn-has-sub ">Services </a>                        
+                            </li>
+                            <!-- End Item With Sub -->
+							
+							<!-- Item With Sub -->
+                          <!--   <li>
+                                <a href="pages-team-2.html" class="mn-has-sub ">Team </a>                        
+                            </li> -->
+                            <!-- End Item With Sub -->
+							
+							<!-- Item With Sub -->
+                           <!--  <li>
+                                <a href="pages-gallery-4col.html" class="mn-has-sub ">Gallery </a>                        
+                            </li> -->
+                            <!-- End Item With Sub -->
+							
+							<!-- Item With Sub -->
+                            <li>
+                                <a href="/contact" class="mn-has-sub ">Contact Us </a>    
+								
+                            </li>
+                            <!-- End Item With Sub -->
+                            
+                          
+                        
+                            
+            
+                            
+                            <!-- Item With Sub -->
+                        <!--     <li>
+                                <a href="portfolio-boxed-gutter-blackhover.html" class="mn-has-sub">Portfolio </a>
+                                
+                          
+                                
+                            </li> -->
+                            <!-- End Item With Sub -->
+                            
+                            <!-- Item With Sub -->
+                          <!--   <li>
+                                <a href="blog-classic-fullwidth.html" class="mn-has-sub">Blog </a>
+                                
+                              
+                                
+                            </li> -->
+                            <!-- End Item With Sub -->
+                            
+                       
+                            
+                            <!-- Divider -->
+                            <li><a>&nbsp;</a></li>
+                            <!-- End Divider -->
+                            
+                
+                            
+                        </ul>
+                    </div>
+                    <!-- End Main Menu -->
+                    
+
+                </div>
+            </nav>
+            <!-- End Navigation panel -->
